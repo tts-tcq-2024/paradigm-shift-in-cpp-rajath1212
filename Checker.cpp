b6738a61ed30a0596619347ef9d2e8da02f4badd
rajath1212 <80102711+rajath1212@users.noreply.github.com>
2024-08-26 11:09:52 +0530

    Update Checker.cpp

--- Checker.cpp
+++ Checker.cpp
@@ -2,27 +2,27 @@
 #include <cassert>
 #include <iostream>
 using namespace std;
-bool temperaturecheck(float temperature1)
+bool temperaturecheck(float temperature)
 {  
-    if(temperature1 < 0 || temperature1 > 45) {
+    if(temperature < 0 || temperature > 45) {
     cout << "Temperature out of range!\n";
     return false;
     }
     else 
         return true;
 }
-bool soccheck(float soc1)
+bool soccheck(float soc)
 {
-  if(soc1 < 20 || soc1 > 80) {
+  if(soc < 20 || soc > 80) {
     cout << "State of Charge out of range!\n";
     return false;
   }
     else 
         return true;
 }
-bool chargeratecheck(float chargeRate1)
+bool chargeratecheck(float chargeRate)
 {
-  if(chargeRate1 > 0.8) {
+  if(chargeRate > 0.8) {
     cout << "Charge Rate out of range!\n";
     return false;
   }
