4580689b51ea086f95f0648acb3d8cc8b976fb36
rajath1212 <80102711+rajath1212@users.noreply.github.com>
2024-08-26 11:15:50 +0530

    Update Checker.cpp

--- Checker.cpp
+++ Checker.cpp
@@ -29,23 +29,14 @@ bool chargeratecheck(float chargeRate)
     else 
         return true;
 }
-bool batteryIsOk(float temperature, float soc, float chargeRate) {
-
-  if((temperaturecheck(temperature)==false)||(soccheck(soc)==false)||(chargeratecheck(chargeRate)==false))
-  {
-    return false;
-  }
-  return true;
-}
 
 int main() {
-  //assert(batteryIsOk(25, 70, 0.7) == true);
+  
    assert(temperaturecheck(25) == true);
    assert(soccheck(70) == true);
    assert(chargeratecheck(0.7) == true);
    assert(temperaturecheck(50) == true);
    assert(soccheck(85) == true);
    assert(chargeratecheck(0) == true);
-  //assert(batteryIsOk(50, 85, 0) == false);
   return 0;
 }
